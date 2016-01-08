0c491f203804797cbd33afa206e2285f27fc270e
Cyrille Pitchen <cyrille.pitchen@atmel.com>
2016-01-08 17:02:21 +0100

    mtd: spi-nor: configure the number of dummy clock cycles on Micron memories
    The spi-nor framework currently expects all Fast Read operations to use 8
    dummy clock cycles. Especially some drivers like m25p80 can only support
    multiple of 8 dummy clock cycles.
    
    On Micron memories, the number of dummy clock cycles to be used by Fast
    Read commands can be safely set to 8 by updating the Volatile
    Configuration Register (VCR).
    
    Also the XIP bit is set at the same time when updating the VCR so the
    Continuous Read mode is disabled: this prevents us from entering it by
    mistake.
    
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@atmel.com>

--- include/linux/mtd/spi-nor.h
+++ include/linux/mtd/spi-nor.h
@@ -86,6 +86,8 @@
 
 /* Used for Micron flashes only. */
 #define SPINOR_OP_MIO_RDID	0xaf	/* Multiple I/O Read JEDEC ID */
+#define SPINOR_OP_RD_VCR	0x85	/* Read VCR register */
+#define SPINOR_OP_WR_VCR	0x81	/* Write VCR register */
 #define SPINOR_OP_RD_EVCR	0x65    /* Read EVCR register */
 #define SPINOR_OP_WD_EVCR	0x61    /* Write EVCR register */
 

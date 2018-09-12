8c89d71e7a6a1e5bbc218c8c96454db5b5873e16
Lucas Stach <l.stach@pengutronix.de>
2018-09-12 15:11:30 +0200

    dmaengine: imx-sdma: don't print warning when firmware is absent
    The SDMA firmware is optional and a usable fallabck to the internal
    ROM firmware is present in the driver. There is already a message
    printed informing the user that the internal firmware is used, so
    there is no need to print a scary warning for what is normal operation
    on most systems.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -2062,7 +2062,7 @@ static int sdma_get_firmware(struct sdma_engine *sdma,
 {
 	int ret;
 
-	ret = request_firmware_nowait(THIS_MODULE,
+	ret = request_firmware_nowait_nowarn(THIS_MODULE,
 			FW_ACTION_UEVENT, fw_name, sdma->dev,
 			GFP_KERNEL, sdma, sdma_load_firmware);
 

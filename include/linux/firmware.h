c34471a4ace401d47a81655ab4f7476612118a7d
Lucas Stach <l.stach@pengutronix.de>
2018-09-12 15:08:31 +0200

    firmware: add nowarn variant of request_firmware_nowait()
    Device drivers with optional firmware may still want to use the
    asynchronous firmware loading interface. To avoid printing a
    warining into the kernel log when the optional firmware is
    absent, add a nowarn variant of this interface.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- include/linux/firmware.h
+++ include/linux/firmware.h
@@ -103,6 +103,10 @@ int request_firmware_nowait(
 	struct module *module, bool uevent,
 	const char *name, struct device *device, gfp_t gfp, void *context,
 	void (*cont)(const struct firmware *fw, void *context));
+int request_firmware_nowait_nowarn(
+	struct module *module, bool uevent,
+	const char *name, struct device *device, gfp_t gfp, void *context,
+	void (*cont)(const struct firmware *fw, void *context));
 int request_firmware_direct(const struct firmware **fw, const char *name,
 			    struct device *device);
 int request_firmware_into_buf(const struct firmware **firmware_p,
@@ -135,6 +139,14 @@ static inline int firmware_request_platform(const struct firmware **fw,
 }
 
 static inline int request_firmware_nowait(
+	struct module *module, bool uevent,
+	const char *name, struct device *device, gfp_t gfp, void *context,
+	void (*cont)(const struct firmware *fw, void *context))
+{
+	return -EINVAL;
+}
+
+static inline int request_firmware_nowait_nowarn(
 	struct module *module, bool uevent,
 	const char *name, struct device *device, gfp_t gfp, void *context,
 	void (*cont)(const struct firmware *fw, void *context))

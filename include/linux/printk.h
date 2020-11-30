a5744e9d8f3037627a61c32da301822f2ac9dec2
John Ogness <john.ogness@linutronix.de>
2020-11-30 01:42:10 +0106

    printk: add pr_flush()
    Provide a function to allow waiting for console printers to catch
    up to the latest logged message.
    
    Use pr_flush() to give console printers a chance to finish in
    critical situations if no atomic console is available. For now
    pr_flush() is only used in the most common error paths:
    panic(), print_oops_end_marker(), report_bug(), kmsg_dump().
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -161,6 +161,8 @@ int vprintk(const char *fmt, va_list args);
 asmlinkage __printf(1, 2) __cold
 int _printk(const char *fmt, ...);
 
+bool pr_flush(int timeout_ms, bool reset_on_progress);
+
 /*
  * Special printk facility for scheduler/timekeeping use only, _DO_NOT_USE_ !
  */
@@ -228,6 +230,11 @@ static inline void printk_deferred_enter(void)
 
 static inline void printk_deferred_exit(void)
 {
+}
+
+static inline bool pr_flush(int timeout_ms, bool reset_on_progress)
+{
+	return true;
 }
 
 static inline int printk_ratelimit(void)

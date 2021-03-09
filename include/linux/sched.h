68cf11cb353d678c646ded9bb80f09995e25a794
Thomas Gleixner <tglx@linutronix.de>
2021-03-09 09:55:53 +0100

    softirq: Add RT specific softirq accounting
    RT requires the softirq processing and local bottomhalf disabled regions to
    be preemptible. Using the normal preempt count based serialization is
    therefore not possible because this implicitely disables preemption.
    
    RT kernels use a per CPU local lock to serialize bottomhalfs. As
    local_bh_disable() can nest the lock can only be acquired on the outermost
    invocation of local_bh_disable() and released when the nest count becomes
    zero. Tasks which hold the local lock can be preempted so its required to
    keep track of the nest count per task.
    
    Add a RT only counter to task struct and adjust the relevant macros in
    preempt.h.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1045,6 +1045,9 @@ struct task_struct {
 	int				softirq_context;
 	int				irq_config;
 #endif
+#ifdef CONFIG_PREEMPT_RT
+	int				softirq_disable_cnt;
+#endif
 
 #ifdef CONFIG_LOCKDEP
 # define MAX_LOCK_DEPTH			48UL

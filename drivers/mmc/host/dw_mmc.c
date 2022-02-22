e27191d3ee0f30bd9a5d3548fe847fb4b961ded3
Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>
2022-02-22 12:12:00 +0100

    mmc: dw_mmc: revert call the dw_mci_prep_stop_abort() by default
    Manually revert upstream commit 613612a2ef1e ("mmc: dw_mmc: call the
    dw_mci_prep_stop_abort() by default") as it is causing ambiguous error
    messages regarding not completed stop transfers to eMMC controller.
    
    Revert to the original behavior where data->stop structure was used.
    
    Signed-off-by: Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -380,7 +380,7 @@ static void dw_mci_start_command(struct dw_mci *host,
 
 static inline void send_stop_abort(struct dw_mci *host, struct mmc_data *data)
 {
-	struct mmc_command *stop = &host->stop_abort;
+	struct mmc_command *stop = data->stop ? data->stop : &host->stop_abort;
 
 	dw_mci_start_command(host, stop, host->stop_cmdr);
 }
@@ -1203,7 +1203,10 @@ static void __dw_mci_start_request(struct dw_mci *host,
 		spin_unlock_irqrestore(&host->irq_lock, irqflags);
 	}
 
-	host->stop_cmdr = dw_mci_prep_stop_abort(host, cmd);
+	if (mrq->stop)
+		host->stop_cmdr = dw_mci_prepare_command(slot->mmc, mrq->stop);
+	else
+		host->stop_cmdr = dw_mci_prep_stop_abort(host, cmd);
 }
 
 static void dw_mci_start_request(struct dw_mci *host,
@@ -1794,7 +1797,8 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			 */
 			if (test_and_clear_bit(EVENT_DATA_ERROR,
 					       &host->pending_events)) {
-				if (!(host->data_status & (SDMMC_INT_DRTO |
+				if (data->stop ||
+				    !(host->data_status & (SDMMC_INT_DRTO |
 							   SDMMC_INT_EBE)))
 					send_stop_abort(host, data);
 				dw_mci_stop_dma(host);
@@ -1831,7 +1835,8 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			 */
 			if (test_and_clear_bit(EVENT_DATA_ERROR,
 					       &host->pending_events)) {
-				if (!(host->data_status & (SDMMC_INT_DRTO |
+				if (data->stop ||
+				    !(host->data_status & (SDMMC_INT_DRTO |
 							   SDMMC_INT_EBE)))
 					send_stop_abort(host, data);
 				dw_mci_stop_dma(host);
@@ -1909,7 +1914,7 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			host->cmd = NULL;
 			host->data = NULL;
 
-			if (!mrq->sbc && mrq->stop)
+			if (mrq->stop)
 				dw_mci_command_complete(host, mrq->stop);
 			else
 				host->cmd_status = 0;

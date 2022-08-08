f8461ff100b4a991584d872bff541404f539838f
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2022-08-08 14:56:00 +0200

    mm/memcg: Add a comment regarding the release `obj'.
    Please fold into
        mm/memcg: Protect memcg_stock with a local_lock_t
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    [do: backported to v5.15]
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>
    Signed-off-by: Clark Williams <williams@redhat.com>
    Signed-off-by: Clark Williams <clrkwllms@kernel.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3255,6 +3255,10 @@ static struct obj_cgroup *drain_obj_stock(struct memcg_stock_pcp *stock)
 	}
 
 	stock->cached_objcg = NULL;
+	/*
+	 * The `old' objects needs to be released by the caller via
+	 * obj_cgroup_put() outside of memcg_stock_pcp::stock_lock.
+	 */
 	return old;
 }
 

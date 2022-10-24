af4355f4bbc813bdf424b72f54390c6e2cf9fe9e
Daniel Wagner <wagi@monom.org>
2022-10-24 10:58:29 +0200

    rcu: Update rcuwait
    This is an all in one commit backporting updates for rcuwait:
     - 03f4b48edae7 ("rcuwait: Annotate task_struct with __rcu")
     - 191a43be61d6 ("rcuwait: Introduce rcuwait_active()")
     - 5c21f7b322cb ("rcuwait: Introduce prepare_to and finish_rcuwait")
     - 80fbaf1c3f29 ("rcuwait: Add @state argument to rcuwait_wait_event()")
     - 9d9a6ebfea32 ("rcuwait: Let rcuwait_wake_up() return whether or not a task was awoken")
     - 58d4292bd037 ("rcu: Uninline multi-use function: finish_rcuwait()")
    
    Signed-off-by: Daniel Wagner <wagi@monom.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -291,8 +291,9 @@ retry:
 	return task;
 }
 
-void rcuwait_wake_up(struct rcuwait *w)
+int rcuwait_wake_up(struct rcuwait *w)
 {
+	int ret = 0;
 	struct task_struct *task;
 
 	rcu_read_lock();
@@ -316,8 +317,10 @@ void rcuwait_wake_up(struct rcuwait *w)
 	 */
 	task = rcu_dereference(w->task);
 	if (task)
-		wake_up_process(task);
+		ret = wake_up_process(task);
 	rcu_read_unlock();
+
+	return ret;
 }
 
 /*

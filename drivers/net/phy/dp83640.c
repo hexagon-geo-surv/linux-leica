a12dfed2330bad1d76973fd927d3749e573b0fe1
Marco Felsch <m.felsch@pengutronix.de>
2022-11-10 18:39:21 +0100

    net: phy: add phy_device_set_miits helper
    Provide a small setter helper to set the mii timestamper. The helper
    detects possible overrides and hides the phydev internal from the
    driver.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

--- drivers/net/phy/dp83640.c
+++ drivers/net/phy/dp83640.c
@@ -1456,7 +1456,7 @@ static int dp83640_probe(struct phy_device *phydev)
 	for (i = 0; i < MAX_RXTS; i++)
 		list_add(&dp83640->rx_pool_data[i].list, &dp83640->rxpool);
 
-	phydev->mii_ts = &dp83640->mii_ts;
+	phy_device_set_miits(phydev, &dp83640->mii_ts);
 	phydev->priv = dp83640;
 
 	spin_lock_init(&dp83640->rx_lock);

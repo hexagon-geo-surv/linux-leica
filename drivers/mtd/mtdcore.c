f787f3ebadd1f0aa91544af4d24185148a8a4d94
Rafał Miłecki <rafal@milecki.pl>
2023-10-23 12:27:59 +0200

    Revert "nvmem: add new config option"
    This reverts commit 517f14d9cf3533d5ab4fded195ab6f80a92e378f.
    
    Config option "no_of_node" is no longer needed since adding a more
    explicit and targeted option "add_legacy_fixed_of_cells".
    
    That "no_of_node" config option was needed *earlier* to help mtd's case.
    
    DT nodes of MTD partitions (that are also NVMEM devices) may contain
    subnodes. Those SHOULD NOT be treated as NVMEM fixed cells.
    
    To prevent NVMEM core code from parsing subnodes a "no_of_node" option
    was added (and set to true in mtd) to make for_each_child_of_node() in
    NVMEM a no-op. That was a bit hacky because it was messing with
    "of_node" pointer to achieve some side-effect.
    
    With the introduction of "add_legacy_fixed_of_cells" config option
    things got more explicit. MTD subsystem simply tells NVMEM when to look
    for fixed cells and there is no need to hack "of_node" pointer anymore.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20231023102759.31529-1-zajec5@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mtd/mtdcore.c
+++ drivers/mtd/mtdcore.c
@@ -578,7 +578,6 @@ static int mtd_nvmem_add(struct mtd_info *mtd)
 	config.read_only = !(mtd->flags & MTD_WRITEABLE);
 	config.root_only = true;
 	config.ignore_wp = true;
-	config.no_of_node = !of_device_is_compatible(node, "nvmem-cells");
 	config.priv = mtd;
 
 	mtd->nvmem = nvmem_register(&config);

86c0541147fae8dad96cb03cb3a56e6d856047bd
Kieran Bingham <kieran.bingham@ideasonboard.com>
2024-01-10 10:12:32 +0000

    media: i2c: imx335: Parse fwnode properties
    Call the V4L2 fwnode device parser to handle controls that are
    standardised by the framework.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- drivers/media/i2c/imx335.c
+++ drivers/media/i2c/imx335.c
@@ -1231,10 +1231,12 @@ static int imx335_init_controls(struct imx335 *imx335)
 {
 	struct v4l2_ctrl_handler *ctrl_hdlr = &imx335->ctrl_handler;
 	const struct imx335_mode *mode = imx335->cur_mode;
+	struct v4l2_fwnode_device_properties props;
 	u32 lpfr;
 	int ret;
 
-	ret = v4l2_ctrl_handler_init(ctrl_hdlr, 7);
+	/* v4l2_fwnode_device_properties can add two more controls */
+	ret = v4l2_ctrl_handler_init(ctrl_hdlr, 9);
 	if (ret)
 		return ret;
 
@@ -1306,6 +1308,15 @@ static int imx335_init_controls(struct imx335 *imx335)
 		v4l2_ctrl_handler_free(ctrl_hdlr);
 		return ctrl_hdlr->error;
 	}
+
+	ret = v4l2_fwnode_device_parse(imx335->dev, &props);
+	if (ret)
+		return ret;
+
+	ret = v4l2_ctrl_new_fwnode_properties(ctrl_hdlr, &imx335_ctrl_ops,
+					      &props);
+	if (ret)
+		return ret;
 
 	imx335->sd.ctrl_handler = ctrl_hdlr;
 

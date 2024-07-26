b0ee5510f94d973065ec474e067427c099ef8e7b
Marco Felsch <m.felsch@pengutronix.de>
2024-07-26 16:31:08 +0200

    usb: hub: add infrastructure to pass onboard_dev port features
    On board devices may require special handling for en-/disable port
    features due to PCB design decisions e.g. enable/disable the VBUS power
    on the port. This commit adds the necessary infrastructure to prepare
    the common code base for such use-cases.
    
    Upstream-Status: Pending
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

--- include/linux/usb/onboard_dev.h
+++ include/linux/usb/onboard_dev.h
@@ -9,10 +9,16 @@ struct list_head;
 #if IS_ENABLED(CONFIG_USB_ONBOARD_DEV)
 void onboard_dev_create_pdevs(struct usb_device *parent_dev, struct list_head *pdev_list);
 void onboard_dev_destroy_pdevs(struct list_head *pdev_list);
+int onboard_dev_port_feature(struct usb_device *udev, bool set, int feature, int port1);
 #else
 static inline void onboard_dev_create_pdevs(struct usb_device *parent_dev,
 					    struct list_head *pdev_list) {}
 static inline void onboard_dev_destroy_pdevs(struct list_head *pdev_list) {}
+static inline int onboard_dev_port_feature(struct usb_device *udev, bool set,
+					   int feature, int port1)
+{
+	return 0;
+}
 #endif
 
 #endif /* __LINUX_USB_ONBOARD_DEV_H */

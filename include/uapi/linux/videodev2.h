023000063b04b8f4fdde9b0726290334cb94ec57
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2024-08-08 22:40:55 +0200

    media: uapi: videodev2: Add V4L2_META_FMT_RK_ISP1_EXT_PARAMS
    The rkisp1 driver stores ISP configuration parameters in the fixed
    rkisp1_params_cfg structure. As the members of the structure are part of
    the userspace API, the structure layout is immutable and cannot be
    extended further. Introducing new parameters or modifying the existing
    ones would change the buffer layout and cause breakages in existing
    applications.
    
    The allow for future extensions to the ISP parameters, introduce a new
    extensible parameters format, with a new format 4CC. Document usage of
    the new format in the rkisp1 admin guide.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Tested-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    (cherry picked from commit 1fc379f6241b331207c4573c4ff43526fe404301)

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -840,6 +840,7 @@ struct v4l2_pix_format {
 /* Vendor specific - used for RK_ISP1 camera sub-system */
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
+#define V4L2_META_FMT_RK_ISP1_EXT_PARAMS	v4l2_fourcc('R', 'K', '1', 'E') /* Rockchip ISP1 3a Extensible Parameters */
 
 #ifdef __KERNEL__
 /*

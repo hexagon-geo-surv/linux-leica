ef123ef140ac7fc2e5c69cb23f2cf6d85e923657
Sascha Hauer <s.hauer@pengutronix.de>
2024-08-20 09:27:10 +0200

    wifi: mwifiex: pass adapter to host sleep functions
    The host sleep functions have effect on the adapter as a whole and
    not to the priv *, so pass the adapter to these functions and use
    mwifiex_adapter_send_cmd() to send the commands.
    
    Upstream-Status: Pending
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/net/wireless/marvell/mwifiex/main.c
+++ drivers/net/wireless/marvell/mwifiex/main.c
@@ -394,8 +394,7 @@ process_start:
 		    !adapter->data_sent &&
 		    !skb_queue_empty(&adapter->tx_data_q)) {
 			if (adapter->hs_activated_manually) {
-				mwifiex_cancel_hs(mwifiex_get_priv(adapter, MWIFIEX_BSS_ROLE_ANY),
-						  MWIFIEX_ASYNC_CMD);
+				mwifiex_cancel_hs(adapter, MWIFIEX_ASYNC_CMD);
 				adapter->hs_activated_manually = false;
 			}
 
@@ -413,8 +412,7 @@ process_start:
 		    !mwifiex_bypass_txlist_empty(adapter) &&
 		    !mwifiex_is_tdls_chan_switching(adapter)) {
 			if (adapter->hs_activated_manually) {
-				mwifiex_cancel_hs(mwifiex_get_priv(adapter, MWIFIEX_BSS_ROLE_ANY),
-						  MWIFIEX_ASYNC_CMD);
+				mwifiex_cancel_hs(adapter, MWIFIEX_ASYNC_CMD);
 				adapter->hs_activated_manually = false;
 			}
 
@@ -431,8 +429,7 @@ process_start:
 		    !adapter->data_sent && !mwifiex_wmm_lists_empty(adapter) &&
 		    !mwifiex_is_tdls_chan_switching(adapter)) {
 			if (adapter->hs_activated_manually) {
-				mwifiex_cancel_hs(mwifiex_get_priv(adapter, MWIFIEX_BSS_ROLE_ANY),
-						  MWIFIEX_ASYNC_CMD);
+				mwifiex_cancel_hs(adapter, MWIFIEX_ASYNC_CMD);
 				adapter->hs_activated_manually = false;
 			}
 

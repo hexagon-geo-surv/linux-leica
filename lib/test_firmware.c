a9e31ee3b7a3cb2f3742683361e6eab3f5faa621
Marco Felsch <m.felsch@pengutronix.de>
2024-09-03 21:16:27 +0200

    firmware_loader: expand firmware error codes with skip error
    Add FW_UPLOAD_ERR_SKIP to allow drivers to inform the firmware_loader
    framework that the update is not required. This can be the case if the
    user provided firmware matches the current running firmware.
    
    Sync lib/test_firmware.c file accordingly.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

--- lib/test_firmware.c
+++ lib/test_firmware.c
@@ -1133,6 +1133,7 @@ static const char * const fw_upload_err_str[] = {
 	[FW_UPLOAD_ERR_RW_ERROR]     = "read-write-error",
 	[FW_UPLOAD_ERR_WEAROUT]	     = "flash-wearout",
 	[FW_UPLOAD_ERR_FW_INVALID]   = "firmware-invalid",
+	[FW_UPLOAD_ERR_SKIP]         = "skip",
 };
 
 static void upload_err_inject_error(struct test_firmware_upload *tst,

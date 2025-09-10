6136676859b78394d5cc145946758cccbb4fd3a2
Stefan Kerkmann <s.kerkmann@pengutronix.de>
2025-09-10 15:03:32 +0200

    wifi: mwifiex: fix endianness handling in mwifiex_send_rgpower_table
    The length of the host command is a u16 stored in little endian byte
    order, which needs byte order conversion to work correctly on big endian
    systems.
    
    Upstream-Status: Pending
    
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Closes: https://lore.kernel.org/linux-wireless/aLqZI4FfOI4iJZtf@stanley.mountain
    Fixes: 7b6f16a25806 ("wifi: mwifiex: add rgpower table loading support")
    Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>
    Reviewed-by: Francesco Dolcini <francesco.dolcini@toradex.com>
    Link: https://patch.msgid.link/20250910-for-next-v1-1-3ee311706231@pengutronix.de
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/marvell/mwifiex/sta_cmd.c
+++ drivers/net/wireless/marvell/mwifiex/sta_cmd.c
@@ -1532,7 +1532,7 @@ int mwifiex_send_rgpower_table(struct mwifiex_adapter *adapter, const u8 *data,
 		}
 
 		if (*pos == '}' && start_raw) {
-			memcpy(&hostcmd->len, &hostcmd->cmd[2], sizeof(u16));
+			hostcmd->len = get_unaligned_le16(&hostcmd->cmd[2]);
 			ret = mwifiex_adapter_send_cmd(adapter, 0, 0, 0,
 						       hostcmd, false);
 			if (ret) {

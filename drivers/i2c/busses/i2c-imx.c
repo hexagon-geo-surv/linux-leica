d7fb8bc05e07bcb562e1f2f621f3b33ce5ab0a91
LI Qingwu <Qing-wu.Li@leica-geosystems.com.cn>
2025-12-24 16:37:49 +0800

    i2c: imx: Fix SMBus block read hang on zero length
    SMBus block read transfers encode the payload length in the first data
    byte. When this first byte is zero, there is no payload and the
    transaction should terminate immediately.
    
    On i.MX, if the first byte of an SMBus block read is zero, the driver
    unconditionally overwrites the state with IMX_I2C_STATE_READ_CONTINUE.
    This causes the state machine to enter an endless read loop, eventually
    overrunning internal buffers and leading to a crash.
    
    At the same time, the controller remains in master receive mode and
    never generates a proper STOP condition, leaving the I2C bus permanently
    busy and preventing any further transfers on the bus.
    
    Fix this by handling the zero-length case explicitly: when the first
    byte is zero, ensure that a clean STOP is generated. In this situation
    the controller is in master receive mode, so it must be switched to
    master transmit mode before stopping. This is done by draining the
    pending received byte from I2DR, setting I2CR_MTX to enter transmit
    mode, waiting briefly for the mode change, and then proceeding with the
    normal STOP sequence.
    
    This change has been tested on i.MX 8M Plus platform.
    
    Upstream-Status: Submitted [https://lkml.org/lkml/2025/12/29/248]
    Signed-off-by: LI Qingwu <Qing-wu.Li@leica-geosystems.com.cn>

--- drivers/i2c/busses/i2c-imx.c
+++ drivers/i2c/busses/i2c-imx.c
@@ -734,6 +734,16 @@ static void i2c_imx_stop(struct imx_i2c_struct *i2c_imx, bool atomic)
 		temp = imx_i2c_read_reg(i2c_imx, IMX_I2C_I2CR);
 		if (!(temp & I2CR_MSTA))
 			i2c_imx->stopped = 1;
+		if ((temp & I2CR_MSTA) && !(temp & I2CR_MTX)) {
+			(void)imx_i2c_read_reg(i2c_imx, IMX_I2C_I2DR);
+			temp |= I2CR_MTX;
+			imx_i2c_write_reg(temp, i2c_imx, IMX_I2C_I2CR);
+			if (atomic)
+				udelay(25);
+			else
+				usleep_range(25, 50);
+			temp = imx_i2c_read_reg(i2c_imx, IMX_I2C_I2CR);
+		}
 		temp &= ~(I2CR_MSTA | I2CR_MTX);
 		if (i2c_imx->dma)
 			temp &= ~I2CR_DMAEN;
@@ -1102,7 +1112,8 @@ static irqreturn_t i2c_imx_master_isr(struct imx_i2c_struct *i2c_imx, unsigned i
 
 	case IMX_I2C_STATE_READ_BLOCK_DATA_LEN:
 		i2c_imx_isr_read_block_data_len(i2c_imx);
-		i2c_imx->state = IMX_I2C_STATE_READ_CONTINUE;
+		if (i2c_imx->state == IMX_I2C_STATE_READ_BLOCK_DATA_LEN)
+			i2c_imx->state = IMX_I2C_STATE_READ_CONTINUE;
 		break;
 
 	case IMX_I2C_STATE_WRITE:
